bbedea49e7e23a331be20748c63e7ea46d4efbdb
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2020-05-11 19:37:26 -0300

    list files

--- tools/images.py
+++ tools/images.py
@@ -1,14 +1,23 @@
-import ffmpeg
+import os, shutil, ffmpeg
 
-image = ffmpeg.input('./images/jpeg-sd/001AAN005121.jpg')
+source_folder = input("Source folder:")
 
 # user insert all .tif files in images/master
+finalizadas = [
+    os.path.join(root, name)
+    for root, dirs, files in os.walk(source_folder)
+    for name in files
+    if "FINALIZADAS" in root
+    if name.endswith((".tif"))
+    if not name.endswith(("v.tif"))
+]
+
+for image in finalizadas:
+    filename = os.path.split(image)[1]
+    shutil.copy2(image, ".\images\master")
 
 # ffmpeg converts files to .jpg in images/jpeg
 
 # pandas creates a dataframe with all images available for a record_name
 
 # pandas saves all data regarding imagens in images/images.csv
-
-print("images script")
-
