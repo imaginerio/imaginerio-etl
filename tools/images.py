741c30d89e1ca42a3e2d7e31a76526ba9c0168e4
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2020-05-11 21:39:23 -0300

    defined save_jpeg function

--- tools/images.py
+++ tools/images.py
@@ -2,7 +2,26 @@ import os, shutil, ffmpeg
 
 source_folder = input("Source folder:")
 
+
+def save_jpeg(image, output_folder, size=None, overwrite=False):
+    stream = ffmpeg.input(image)
+    filename, ext = (os.path.split(image)[1]).split(".")
+
+    if not overwrite and os.path.exists(os.path.join(output_folder, filename)):
+        return print("File already exists")
+
+    if size:
+        stream = ffmpeg.filter(
+            stream, "scale", size, size, force_original_aspect_ratio="decrease"
+        )
+    stream = ffmpeg.output(
+        stream, os.path.join(output_folder, filename) + ".jpg", **{"q": 0}
+    )
+    ffmpeg.run(stream)
+
+
 # user insert all .tif files in images/master
+
 finalizadas = [
     os.path.join(root, name)
     for root, dirs, files in os.walk(source_folder)
@@ -13,10 +32,16 @@ finalizadas = [
 ]
 
 for image in finalizadas:
-    filename = os.path.split(image)[1]
-    shutil.copy2(image, ".\images\master")
+    if not os.path.exists(f"./images/master/{os.path.split(image)[1]}"):
+        shutil.copy2(image, "./images/master")
+    else:
+        print("File already in folder")
+    save_jpeg(image, "./images/jpeg-sd", size=1000)
+    save_jpeg(image, "./images/jpeg-hd")
 
 # ffmpeg converts files to .jpg in images/jpeg
+
+# scale=1000:1000:force_original_aspect_ratio=decrease
 
 # pandas creates a dataframe with all images available for a record_name
 
