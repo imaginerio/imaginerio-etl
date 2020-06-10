6e8eba41e2719597d5aca28e9ba85271544d7afa
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2020-06-10 11:20:28 -0300

    Add img_to_commons function

--- tools/modules/export.py
+++ tools/modules/export.py
@@ -1,7 +1,8 @@
+import shutil
 from bokeh.plotting import output_file, show
 from bokeh.layouts import column
-
-from modules import report, maps
+import pandas as pd
+import report, maps
 
 
 def dashboard(METADATA_PATH):
@@ -30,3 +31,13 @@ def dashboard(METADATA_PATH):
 
 def omeka_csv():
     pass
+
+def commons_csv(METADATA_PATH, copy_to):
+    final_df = pd.read_csv(METADATA_PATH)
+    commons_df = pd.DataFrame(final_df[final_df['geometry'].notna() & final_df['img_hd'].notna() & final_df['wikidata_image'].isna()])
+    
+    for id in commons_df['id']:
+        shutil.copy2(f"./images/jpeg-hd/{id}.jpg", copy_to)        
+
+commons_csv("./metadata/metadata.csv", "/Users/martimpassos/Pictures/Commons")
+
