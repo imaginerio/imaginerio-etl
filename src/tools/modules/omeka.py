548e3e45902960ba4069ef4d5dd4268f270a5e57
mechlerc <mechler.cintia@gmail.com>
2020-07-13 18:07:54 -0300

    Number of items published to Omeka added to the dashboard

--- src/tools/modules/omeka.py
+++ src/tools/modules/omeka.py
@@ -1,16 +1,17 @@
 import requests
 
-results = []
+def request_omeka ():
+    results = []
 
-response = requests.get(os.environ['OMEKA_API_URL'], params={'page': page, 'per_page': 200, 'sort_by': 'id', 'sort_order': 'asc'} )
-headers = response.headers
-last_page = int(headers['Link'].split(',')[2].split(";")[0].split('=')[-1].strip('>'))
+    response = requests.get(os.environ['OMEKA_API_URL'], params={'page': page, 'per_page': 200, 'sort_by': 'id', 'sort_order': 'asc'} )
+    headers = response.headers
+    last_page = int(headers['Link'].split(',')[2].split(";")[0].split('=')[-1].strip('>'))
 
 
-for page in range(2, last_page+1):
-    response = requests.get(os.environ['OMEKA_API_URL'], params={'page': page, 'per_page': 200, 'sort_by': 'id', 'sort_order': 'asc'}).json()
-    for item in response:
-        code = item['dcterms:identifier'][0]['@value']
-        results.append(code)
+    for page in range(2, last_page+1):
+        response = requests.get(os.environ['OMEKA_API_URL'], params={'page': page, 'per_page': 200, 'sort_by': 'id', 'sort_order': 'asc'}).json()
+        for item in response:
+            code = item['dcterms:identifier'][0]['@value']
+            results.append(code)
 
-print(results)
+    return results
