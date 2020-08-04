995c36a4dd81e6a65591cf0eefdd95fcfc50f418
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2020-08-04 17:49:37 -0300

    Define update_data function

--- src/tools/modules/omeka.py
+++ src/tools/modules/omeka.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import os, re, requests
 from time import sleep
-
-# from requests.adapters import HTTPAdapter
-# from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 
 def load(endpoint):
--- src/tools/update_data.py
+++ src/tools/update_data.py
@@ -6,6 +6,32 @@ from tqdm import tqdm
 from modules import camera, catalog, images, wikidata, portals, export, omeka
 
 
+def update_metadata(catalog, dataframes):
+    final_df = catalog
+    review_df = pd.DataFrame(columns=["id", "_merge"])
+    for dataframe in dataframes:
+        final_df = catalog
+        final_df = pd.merge(
+            final_df, dataframe, on=["id"], how="left", validate="one_to_one",
+        )
+        missing = pd.merge(
+            catalog,
+            dataframe,
+            on="id",
+            how="outer",
+            indicator="Not in",
+            validate="one_to_one",
+            copy=True,
+        )
+        missing = missing[missing["Not in"] == "left_only"]
+        missing["Not in"] = f"{dataframe.name}"
+        review_df = review_df.append(missing[["id", "Not in"]], ignore_index=True)
+    review_df = review_df.groupby("id", as_index=False).agg(list)
+    review_df.to_csv(os.environ["REVIEW_PATH"], index=False)
+    final_df.to_csv(os.environ["METADATA_PATH"], index=False)
+    return final_df, review_df
+
+
 def main():
     """
     Execute all functions
@@ -17,49 +43,50 @@ def main():
 
             pbar.set_description("Loading Image Paths")
             images_df = images.load(os.environ["IMAGES_PATH"])
-            pbar.update(5)
+            images_df.name = "Images"
+            pbar.update(10)
 
             pbar.set_description("Loading Camera Positions")
             camera_df = camera.load(os.environ["CAMERA_PATH"])
-            pbar.update(5)
+            camera_df.name = "Camera"
+            pbar.update(10)
 
             pbar.set_description("Loading Cumulus Metadata")
-            catalog_df = catalog.load(os.environ["CUMULUS_PATH"])
+            catalog_df = catalog.load(os.environ["CUMULUS_XML"])
+            catalog_df.name = "Catalog"
             pbar.update(10)
 
             pbar.set_description("Checking Cumulus Portals")
             portals_df = portals.load(os.environ["PORTALS_PATH"])
-            pbar.update(15)
+            portals_df.name = "Portals"
+            pbar.update(10)
 
             pbar.set_description("Checking Wikidata")
             wikidata_df = wikidata.load(os.environ["WIKIDATA_PATH"])
-            pbar.update(15)
+            wikidata_df.name = "Wikidata"
+            pbar.update(10)
 
             pbar.set_description("Checking Omeka")
             omeka_df = omeka.load(os.environ["OMEKA_API_URL"])
-            pbar.update(15)
+            omeka_df.name = "Omeka"
+            pbar.update(10)
 
-            pbar.set_description("Updating Metadata File")
-            dataframes = [portals_df, images_df, camera_df, wikidata_df, omeka_df]
-            final_df = catalog_df
-            for dataframe in dataframes:
-                final_df = pd.merge(
-                    final_df, dataframe, on=["id"], how="left", validate="one_to_one",
-                )
-                pbar.update(5)
-            final_df.to_csv(os.environ["METADATA_PATH"], index=False)
-
-            pbar.set_description("Updating Dashboard")
-            export.dashboard(os.environ["METADATA_PATH"], PBAR=pbar)
-            pbar.update(25)
+            pbar.set_description("Updating Metadata Files")
+            dataframes = [images_df, camera_df, portals_df, wikidata_df, omeka_df]
+            update_metadata(catalog_df, dataframes)
+            pbar.update(10)
 
             pbar.set_description("Exporting omeka.csv")
             export.omeka_csv(os.environ["METADATA_PATH"])
-            pbar.update(15)
+            pbar.update(10)
 
             pbar.set_description("Exporting gis.csv")
             export.gis_csv(os.environ["METADATA_PATH"])
-            pbar.update(15)
+            pbar.update(10)
+
+            pbar.set_description("Updating Dashboard")
+            export.dashboard(os.environ["METADATA_PATH"], PBAR=pbar)
+            pbar.update(10)
 
             pbar.set_description("Done")
             pbar.close()
