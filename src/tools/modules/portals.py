7e5e90669c65de79510715bb80d66528d33cf165
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2020-08-13 11:52:04 -0300

    Change portals_id to string

--- src/tools/modules/portals.py
+++ src/tools/modules/portals.py
@@ -26,7 +26,7 @@ def load(PATH):
 
             params = urllib.parse.urlencode(payload, quote_via=urllib.parse.quote)
 
-            response = requests.post(os.environ['PORTALS_API_URL'], params=params)
+            response = requests.post(os.environ["PORTALS_API_URL"], params=params)
 
             data = response.json()
 
@@ -46,7 +46,9 @@ def load(PATH):
 
         dataframe["id"] = dataframe["id"].str.split(".", n=1, expand=True)
 
-        dataframe["portals_url"] = os.environ['PREFIX'] + dataframe["portals_id"].astype(str)
+        dataframe["portals_id"] = dataframe["portals_id"].astype(str)
+
+        dataframe["portals_url"] = os.environ["PREFIX"] + dataframe["portals_id"]
 
         dataframe = dataframe[
             [
