0e5659a57004b3e6a91cee8756f8367061ac2604
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2020-08-21 14:03:31 -0300

    Add comments

--- src/tools/modules/upload.py
+++ src/tools/modules/upload.py
@@ -2,9 +2,11 @@ import os
 import boto3
 import pandas as pd
 
+# list geolocated items
 camera = pd.read_csv("./src/metadata/camera/camera.csv")
 geolocated = [f"{name}.jpg" for name in camera["name"]]
 
+# start session
 session = boto3.session.Session()
 client = session.client(
     "s3",
@@ -14,17 +16,22 @@ client = session.client(
     aws_secret_access_key=os.environ["DIGITALOCEAN_SECRET_KEY"],
 )
 
+# list items alredy on cloud
 response = client.list_objects_v2(
     Bucket="rioiconography", MaxKeys=4000, Prefix="situatedviews/"
 )
-
 cloud = [item["Key"] for item in response["Contents"]]
+
+# list items on local folder
 target = os.environ["JPEG_HD"]
 contents = os.listdir(target)
+
+# filter items to be uploaded
 contents = [
     x for x in contents if f"situatedviews/{x}" not in cloud and x in geolocated
 ]
 
+# upload files
 for index, filename in enumerate(contents):
     try:
         client.upload_file(
