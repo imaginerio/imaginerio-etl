8485da3328976a9bb7e216872f4b873648776453
mechlerc <mechler.cintia@gmail.com>
2020-10-05 10:38:16 -0300

    modified map points

--- src/modules/maps.py
+++ src/modules/maps.py
@@ -80,7 +80,7 @@ def apply_transform(geodf):
     # reduce columns and return final geodataframe
 
     map_geodf = geodf[
-        ["id", "title", "creator", "img_hd", "img_sd", "geometry", "lat2", "lng2"]
+        ["id", "title", "creator", "img_sd", "geometry", "lat2", "lng2"]
     ]
 
     return map_geodf
@@ -88,16 +88,11 @@ def apply_transform(geodf):
 
 def update_map(map_geodf):
 
-    # filter map_geodf
-    map_geodf_img = map_geodf.copy().dropna(subset=["img_hd"])
-    map_geodf_noimg = map_geodf.copy()[map_geodf["img_hd"].isna()]
-
     # create a geodatasource
-    geosource_img = GeoJSONDataSource(geojson=map_geodf_img.to_json())
-    geosource_noimg = GeoJSONDataSource(geojson=map_geodf_noimg.to_json())
+    geosource = GeoJSONDataSource(geojson=map_geodf.to_json())
 
     # Description from points
-    TOOLTIPS1 = """
+    TOOLTIPS = """
         <div style="margin: 5px; width: 300px" >
         <img
             src="@img_sd" alt="@img_sd" height=200
@@ -107,11 +102,6 @@ def update_map(map_geodf):
             <h3 style='font-size: 10px; font-weight: bold;'>@id</h3>
             <p style='font-size: 10px; font-weight: light; font-style: italic;'>@creator</p>
         
-        </div>
-    """
-    TOOLTIPS2 = """
-        <div style="margin: 5px; width: 300px" >
-            <h3 style='font-size: 10px; font-weight: bold;'>@id</h3>
         </div>
     """
 
@@ -131,7 +121,7 @@ def update_map(map_geodf):
     viewcone = maps.patches(
         xs="xs",
         ys="ys",
-        source=geosource_img,
+        source=geosource,
         fill_color="white",
         fill_alpha=0,
         line_color=None,
@@ -140,57 +130,21 @@ def update_map(map_geodf):
         hover_line_color="grey",
     )
 
-    point_noimg = maps.circle(
+    point = maps.circle(
         x="lat2",
         y="lng2",
-        source=geosource_noimg,
-        size=7,
-        fill_color="gainsboro",
-        fill_alpha=0.5,
-        line_color="dimgray",
-        legend_label="No HD Images",
-    )
-
-    point_img = maps.circle(
-        x="lat2",
-        y="lng2",
-        source=geosource_img,
+        source=geosource,
         size=7,
         fill_color="orange",
         fill_alpha=0.5,
-        line_color="dimgray",
-        legend_label="HD Images",
+        line_color="dimgray"
     )
 
     # create a hovertool
     h1 = HoverTool(renderers=[viewcone], tooltips=None, mode="mouse", show_arrow=False)
-    h2 = HoverTool(
-        renderers=[point_img], tooltips=TOOLTIPS1, mode="mouse", show_arrow=False
-    )
-    h3 = HoverTool(
-        renderers=[point_noimg], tooltips=TOOLTIPS2, mode="mouse", show_arrow=False
-    )
+    h2 = HoverTool(renderers=[point], tooltips=TOOLTIPS, mode="mouse", show_arrow=False)
 
-    # create a legend
-    dic = {"HD Images": len(map_geodf_img), "No HD Images": len(map_geodf_noimg)}
-
-    datas = pd.Series(dic).reset_index(name="value").rename(columns={"index": "data"})
-
-    sep = []
-
-    for i in range(len(datas.index)):
-        sep.append(": ")
-
-    datas["legend"] = datas["data"] + sep + datas["value"].astype(str)
-
-    legend = Legend(
-        items=[(datas.iloc[0, 2], [point_img]), (datas.iloc[1, 2], [point_noimg])],
-        location="top_right",
-        orientation="vertical",
-    )
-
-    maps.add_tools(h1, h2, h3)
-    maps.add_layout(legend)
+    maps.add_tools(h1, h2)
     maps.toolbar.active_scroll = maps.select_one(WheelZoomTool)
     maps.xaxis.major_tick_line_color = None
     maps.xaxis.minor_tick_line_color = None
