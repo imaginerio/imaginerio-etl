dfcec6dd0c7c42ffed6b37feb7b61e2e3724ba1a
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2020-10-06 09:23:09 -0300

    Change from ffmpeg to PIL, IPTCInfo3 to exiftool

--- src/modules/update_images.py
+++ src/modules/update_images.py
@@ -3,23 +3,17 @@ import shutil
 
 import ffmpeg
 import pandas as pd
+from PIL import Image as PILImage
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
 # path to images source on remote drive
-SOURCE = input("Source folder:")
+SOURCE = os.environ["SOURCE"]
 
 # list geolocated items
-camera = pd.read_csv("./src/metadata/camera/camera.csv")
+camera = pd.read_csv("src/" + os.environ["CAMERA_CSV"])
 geolocated = list(camera["name"])
-
-# write rclone filter file
-f = open(os.environ["RCLONE"], "w+")
-for id in geolocated:
-    f.write(f"+ {id}.jpg\n")
-f.write("- *")
-f.close()
 
 
 class Image:
@@ -33,78 +27,67 @@ class Image:
         self.id = str(os.path.split(self.path)[1].split(".")[0])
 
 
-def save_jpeg(image, output_folder, size=None, overwrite=False):
-    """
-    Saves jpg file using ffmpeg or returns None if file already exists and overwrite=False.
-    If rescaling, "size" is the largest dimension in resulting image. 
-    """
-
-    # initialize ffmpeg
-    stream = ffmpeg.input(image)
-    # get filename for full path
-    filename = (os.path.split(image)[1]).split(".")[0]
-    # skip existing files when overwrite is False
-    if not overwrite and os.path.exists(os.path.join(output_folder, f"{filename}.jpg")):
-        print(f"{filename}.jpg already exists")
-        return None
-    # fit image within a square of size 'size'
-    if size:
-        stream = ffmpeg.filter(
-            stream, "scale", size, size, force_original_aspect_ratio="decrease"
-        )
-    # destination folder
-    stream = ffmpeg.output(
-        stream, os.path.join(output_folder, filename) + ".jpg", **{"q": 0}
-    )
-    # run ffmpeg
-    ffmpeg.run(stream, overwrite_output=True)
-
-
 def file_handler(source_folder):
     """
     Returns list of original files in source folder according to internal requirements, 
     copies them to master and saves jpegs.
     """
 
-    # create list of Image objects from source
     files = [
         Image(os.path.join(root, name))
-        for root, dirs, files in os.walk(source_folder)
+        for root, dirs, files in os.walk(os.environ["SOURCE"])
         for name in files
         if "FINALIZADAS" in root
         and name.endswith((".tif"))
         and not name.endswith(("v.tif"))
     ]
 
-    # iterate over files at source
-    for image in files:
-        # copy original tiffs to /tiff/ or skip if already there
-        if not os.path.exists(os.path.join(os.environ["TIFF"], image.tif)):
-            shutil.copy2(image.path, os.environ["TIFF"])
+    for infile in files:
+
+        if not os.path.exists(os.path.join(os.environ["TIFF"], infile.tif)):
+            shutil.copy2(infile.path, os.environ["TIFF"])
         else:
-            print(f"{image.tif} already in folder")
-        # create hd and sd jpegs for geolocated items
-        if image.id in geolocated:
-            if not os.path.exists("D:\imagineRio-images\jpeg-hd\{image.jpg}"):
-                os.system(f"magick D:\imagineRio-images\tiff\{image.tif} -delete 1 -quality 100 -format jpg D:\imagineRio-images\jpeg-hd\{image.jpg}")
-                #save_jpeg(
-                #    os.path.join(os.environ["TIFF"], image.tif), os.environ["JPEG_HD"]
-                #)
-                os.system(f"magick D:\imagineRio-images\tiff\{image.tif}[1000x1000] -delete 1 -quality 100 -format jpg D:\imagineRio-images\jpeg-sd\{image.jpg}")
-                #save_jpeg(
-                #    os.path.join(os.environ["TIFF"], image.tif),
-                #    os.environ["JPEG_SD"].replace(".", "src"),
-                #    size=1000,
-                #)
+            print(f"{infile.tif} already copied")
+
+        if infile.id in geolocated:
+            hdout = os.path.join("src", os.environ["JPEG_HD"], infile.jpg)
+            sdout = os.path.join("src", os.environ["JPEG_SD"], infile.jpg)
+            size = (1000, 1000)
+            if not os.path.exists(hdout):
+                try:
+                    with PILImage.open(
+                        os.path.join(os.environ["TIFF"], infile.tif)
+                    ) as im:
+                        im.save(hdout)
+                except OSError:
+                    print("cannot convert", infile.tif)
+            else:
+                print("HD version already exists")
+            if not os.path.exists(sdout):
+                try:
+                    with PILImage.open(
+                        os.path.join(os.environ["TIFF"], infile.tif)
+                    ) as im:
+                        im.thumbnail(size)
+                        im.save(sdout)
+                except OSError:
+                    print("cannot create thumbnail for", infile.tif)
+            else:
+                print("SD version already exists")
         else:
-            if not os.path.exists("D:\imagineRio-images\jpeg-sd\backlog\{image.jpg}"):
-                # create backlog with sd jpegs to be geolocated
-                os.system(f"magick D:\imagineRio-images\tiff\{image.tif}[1000x1000] -quality 100 -format jpg D:\imagineRio-images-2\jpeg-sd\backlog\{image.jpg}")
-                #save_jpeg(
-                #    os.path.join(os.environ["TIFF"], image.tif),
-                #    os.environ["IMG_BACKLOG"],
-                #    size=1000,
-            #)
+            backlog = os.path.join("src", os.environ["IMG_BACKLOG"], infile.jpg)
+            size = (1000, 1000)
+            if not os.path.exists(backlog):
+                try:
+                    with PILImage.open(
+                        os.path.join(os.environ["TIFF"], infile.tif)
+                    ) as im:
+                        im.thumbnail(size)
+                        im.save(backlog)
+                except OSError:
+                    print("cannot create backlog thumbnail for", infile.tif)
+            else:
+                print("Backlog version already exists")
     return files
 
 
@@ -147,12 +130,14 @@ def create_images_df(files):
                     "img_sd": os.path.join(os.environ["GITHUB"], image[0].jpg),
                 }
                 for i in image[1:]:
-                    item[f"{i.id[-1]}"] = i.jpg
+                    item[f" {i.id[-1]}"] = i.jpg
             else:
-                # if item isn't geolocated, don't send to github or cloud
+                # if item isn't geolocated, send to github backlog
                 item = {
                     "id": image[0].id,
-                    "img_sd": os.path.join(os.environ["GITHUB"], backlog, image[0].jpg),
+                    "img_sd": os.path.join(
+                        os.environ["GITHUB"], "backlog", image[0].jpg
+                    ),
                 }
         else:
             if image.id in geolocated:
@@ -162,10 +147,10 @@ def create_images_df(files):
                     "img_sd": os.path.join(os.environ["GITHUB"], image.jpg),
                 }
             else:
-                # if item isn't geolocated, don't send to github or cloud
+                # if item isn't geolocated, send to github backlog
                 item = {
                     "id": image.id,
-                    "img_sd": os.path.join(os.environ["GITHUB"], backlog, image.jpg),
+                    "img_sd": os.path.join(os.environ["GITHUB"], "backlog", image.jpg),
                 }
         items.append(item)
 
@@ -185,7 +170,7 @@ def main():
 
     print(images_df.head())
 
-    images_df.to_csv(os.environ["IMAGES"], index=False)
+    images_df.to_csv(os.path.join("src", os.environ["IMAGES"]), index=False)
 
 
 if __name__ == "__main__":
