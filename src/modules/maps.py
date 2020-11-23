6ab8be4632ac0f5ff7de38e123a7594cf72bd653
unknown <mechler.cintia@gmail.com>
2020-11-23 17:53:32 -0300

    map created with search bar by record name

--- src/modules/maps.py
+++ src/modules/maps.py
@@ -2,16 +2,20 @@ import json
 
 import geopandas as gpd
 import pandas as pd
+import numpy as np
 from bokeh.models import (
     Circle,
     GeoJSONDataSource,
     HoverTool,
-    Legend,
     Patches,
     WheelZoomTool,
     CustomJS,
+    CustomJSFilter,
+    TextInput,
+    CDSView
 )
 from bokeh.plotting import figure
+from bokeh.layouts import column, layout
 from bokeh.tile_providers import Vendors, get_provider
 from pyproj import Proj, transform
 from shapely import wkt
@@ -30,8 +34,11 @@ def update(PATH):
         # transform map projection
         map_geodf = apply_transform(geodf)
 
-        # update map
-        export_map = update_map(map_geodf)
+        # update maps
+        map1 = index_map(map_geodf)
+        map2 = search_map(map_geodf)
+
+        export_map = {"index":map1,"search":map2}
 
         return export_map
 
@@ -86,7 +93,7 @@ def apply_transform(geodf):
     return map_geodf
 
 
-def update_map(map_geodf):
+def index_map(map_geodf):
 
     # filter map_geodf
     map_geodf_img = map_geodf.copy().dropna(subset=["img_sd"])
@@ -115,7 +122,7 @@ def update_map(map_geodf):
                 <p style='font-size: 10px; font-weight: light; font-style: italic;'>@creator</p>
             </div>
         """
-    callback_tp = CustomJS(
+    callback_tp1 = CustomJS(
         code="""
                                 var tooltips = document.getElementsByClassName("bk-tooltip");
                                 for (var i = 0, len = tooltips.length; i < len; i ++) {
@@ -128,7 +135,7 @@ def update_map(map_geodf):
     )
 
     # Base map
-    maps = figure(
+    fig1 = figure(
         x_axis_type="mercator",
         y_axis_type="mercator",
         plot_width=1400,
@@ -136,11 +143,11 @@ def update_map(map_geodf):
         toolbar_location=None,
     )
 
-    tile_provider = get_provider(Vendors.CARTODBPOSITRON_RETINA)
-    maps.add_tile(tile_provider)
+    tile_provider1 = get_provider(Vendors.CARTODBPOSITRON_RETINA)
+    fig1.add_tile(tile_provider1)
 
     # construct points and wedges from hover
-    viewcone1 = maps.patches(
+    viewcone1 = fig1.patches(
         xs="xs",
         ys="ys",
         source=geosource_img,
@@ -152,7 +159,7 @@ def update_map(map_geodf):
         hover_line_color="grey",
     )
 
-    viewcone2 = maps.patches(
+    viewcone2 = fig1.patches(
         xs="xs",
         ys="ys",
         source=geosource_noimg,
@@ -164,7 +171,7 @@ def update_map(map_geodf):
         hover_line_color="grey",
     )
 
-    point_noimg = maps.circle(
+    point_noimg = fig1.circle(
         x="lat2",
         y="lng2",
         source=geosource_noimg,
@@ -174,7 +181,7 @@ def update_map(map_geodf):
         line_color="dimgray",
     )
 
-    point_img = maps.circle(
+    point_img = fig1.circle(
         x="lat2",
         y="lng2",
         source=geosource_img,
@@ -190,23 +197,147 @@ def update_map(map_geodf):
         tooltips=TOOLTIPS1,
         mode="mouse",
         show_arrow=False,
-        callback=callback_tp,
+        callback=callback_tp1,
     )
     h2 = HoverTool(
         renderers=[point_noimg],
         tooltips=TOOLTIPS2,
         mode="mouse",
         show_arrow=False,
-        callback=callback_tp,
+        callback=callback_tp1,
     )
 
-    maps.add_tools(h1, h2)
-    maps.toolbar.active_scroll = maps.select_one(WheelZoomTool)
-    maps.xaxis.major_tick_line_color = None
-    maps.xaxis.minor_tick_line_color = None
-    maps.yaxis.major_tick_line_color = None
-    maps.yaxis.minor_tick_line_color = None
-    maps.xaxis.major_label_text_font_size = "0pt"
-    maps.yaxis.major_label_text_font_size = "0pt"
+    fig1.add_tools(h1, h2)
+    fig1.toolbar.active_scroll = fig1.select_one(WheelZoomTool)
+    fig1.xaxis.major_tick_line_color = None
+    fig1.xaxis.minor_tick_line_color = None
+    fig1.yaxis.major_tick_line_color = None
+    fig1.yaxis.minor_tick_line_color = None
+    fig1.xaxis.major_label_text_font_size = "0pt"
+    fig1.yaxis.major_label_text_font_size = "0pt"
 
-    return maps
+    return fig1
+
+def search_map(map_geodf):
+
+    # replace NAN values for string
+    #map_geodf["img_sd"].fillna(value="No img", inplace=True)
+    #print(map_geodf["img_sd"])
+    map_geodf2 = map_geodf.copy().dropna(subset=["img_sd"])
+
+    # create a geodatasource
+    geosource = GeoJSONDataSource(geojson=map_geodf2.to_json())
+
+    # description from points
+    TOOLTIPS = """
+            <div style="margin: 5px; width: 300px" >
+            <img
+                src="@img_sd" alt="@img_sd" height=200
+                style="margin: 0px;"
+                border="2"
+                ></img>
+                <h3 style='font-size: 10px; font-weight: bold;'>@id</h3>
+                <p style='font-size: 10px; font-weight: light; font-style: italic;'>@creator</p>
+            
+            </div>
+        """
+    callback_tp2 = CustomJS(
+        code="""
+                                var tooltips = document.getElementsByClassName("bk-tooltip");
+                                for (var i = 0, len = tooltips.length; i < len; i ++) {
+                                    tooltips[i].style.top = ""; 
+                                    tooltips[i].style.right = "";
+                                    tooltips[i].style.bottom = "0px";
+                                    tooltips[i].style.left = "0px";
+                                }
+                                """
+    )
+    
+    # base map
+    fig2 = figure(
+        x_axis_type="mercator",
+        y_axis_type="mercator",
+        plot_width=1400,
+        plot_height=900,
+        toolbar_location=None)
+
+    tile_provider2 = get_provider(Vendors.CARTODBPOSITRON_RETINA)
+    fig2.add_tile(tile_provider2)
+
+    # search bar
+    search = TextInput(placeholder = "Enter filter")
+
+    # callback triggers the filter when the text input changes
+    custom_filter = CustomJSFilter(args=dict(search=search), code="""
+    var indices = [];
+    if(search.value == ''){
+        for (var i = 0; i < source.get_length(); i++){
+            indices.push(true);
+            }
+    }else{
+        indices = [];
+        for (var i = 0; i < source.get_length(); i++){
+            if (source.data['id'][i] == search.value){
+                indices.push(true);
+            } else {
+                indices.push(false);
+                }
+        }
+    }
+    return indices;
+    """)
+
+    # filter data from input
+    view = CDSView(source=geosource, filters=[custom_filter])
+    search.js_on_change(
+        'value',
+        CustomJS(
+            args=dict(source=geosource),
+            code="""source.change.emit()""")
+            )
+
+    # construct points and wedges from hover
+    viewcone = fig2.patches(
+        xs="xs",
+        ys="ys",
+        source=geosource,
+        fill_color="white",
+        fill_alpha=0,
+        line_color=None,
+        hover_alpha=0.7,
+        hover_fill_color="grey",
+        hover_line_color="grey",
+    )
+
+    point = fig2.circle(
+        x="lat2",
+        y="lng2",
+        source=geosource,
+        size=7,
+        fill_color="orange",
+        fill_alpha=0.5,
+        line_color="dimgray",
+        view=view
+    )
+
+    # create a hovertool
+    ht = HoverTool(
+        renderers=[point],
+        tooltips=TOOLTIPS,
+        mode="mouse",
+        show_arrow=False,
+        callback=callback_tp2,
+    )
+
+    fig2.add_tools(ht)
+    fig2.toolbar.active_scroll = fig2.select_one(WheelZoomTool)
+    fig2.xaxis.major_tick_line_color = None
+    fig2.xaxis.minor_tick_line_color = None
+    fig2.yaxis.major_tick_line_color = None
+    fig2.yaxis.minor_tick_line_color = None
+    fig2.xaxis.major_label_text_font_size = "0pt"
+    fig2.yaxis.major_label_text_font_size = "0pt"
+
+    layout = column(search, fig2)
+
+    return layout
