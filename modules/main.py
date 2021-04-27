8635c2c03a0d137ae21ee626dea1f3a73f18fe45
unknown <mechler.cintia@gmail.com>
2021-04-27 17:25:01 -0300

    Fixed config.yaml problems and delete catalog.py from src/modules

--- modules/main.py
+++ modules/main.py
@@ -1,6 +1,7 @@
 import dagster as dg
+import pandas as pd
 from modules.utilities import read_csv, save_csv
-#from modules.catalog import catalog_main
+from modules.catalog import catalog_main
 
 read_images = read_csv.alias("images")
 read_camera = read_csv.alias("camera")
@@ -12,11 +13,11 @@ def main():
     images_df = read_images()
     camera_df = read_camera()
     #camera_geojson = read_geojson()
-    #catalog_df= catalog_main()
+    catalog_df= catalog_main()
     # metadata_df = merge(images_csv, camera_csv)
     # save_csv(metadata_df)
     # omeka = omeka(metadata)
     # wiki = wiki(metadata)
     save_csv(images_df)
     save_csv(camera_df)
-    #save_csv(catalog_df)
+    save_csv(catalog_df)
