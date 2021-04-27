9625d34299dbb21260e7cc86871a950fee865f5b
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2021-04-27 08:52:46 -0300

    Use env variables instead of config.yaml

--- src/modules/utilities.py
+++ src/modules/utilities.py
@@ -1,18 +1,34 @@
-import numpy as np
 import pandas as pd
 import dagster as dg
+from dotenv import load_dotenv
 
-#objetivo: ler qualquer csv e retornar um df 
+load_dotenv(override=True)
 
-@dg.solid
+
+@dg.solid(config_schema=dg.StringSource)
 def read_csv(context):
     path = context.solid_config
     df = pd.read_csv(path)
     return df
 
+
+run_config = {
+    "solids": {
+        "camera": {"config": {"env": "CAMERA_CSV"}},
+        "images": {"config": {"env": "IMAGES"}},
+    }
+}
+
+read_camera = read_csv.alias("camera")
+read_images = read_csv.alias("images")
+
+
 @dg.pipeline
 def utilities_main():
-    read_camera = read_csv.alias("camera")
-    read_images = read_csv.alias("images")
     read_camera()
     read_images()
+
+
+if __name__ == "__main__":
+    result = dg.execute_pipeline(utilities_main, run_config=run_config)
+
