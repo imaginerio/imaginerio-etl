23b9af1e42a3df78b2ed3609b0fe62d446e11609
PitangaVigand <pitangavigand@gmail.com>
2021-05-14 19:20:46 -0300

    Create solids and startig pipeline schema of export module

--- bin/pipelines/apis_pipeline.py
+++ bin/pipelines/apis_pipeline.py
@@ -8,19 +8,19 @@ from bin.solids.apis import portals_dataframe, query_portals, query_wikidata, qu
 
 @dg.pipeline(mode_defs =[dg.ModeDefinition(resource_defs={"pandas_csv":df_csv_io_manager, "metadata_root":root_input, "slack": slack_resource})])
 def apis(): 
-    slack_solid()
+    #slack_solid()
 
-    #omeka_results = query_omeka()
-    #omeka_df = omeka_dataframe(omeka_results)
+    omeka_results = query_omeka()
+    omeka_df = omeka_dataframe(omeka_results)
     #merge_dfs(omeka_df)
    
 
-    #wikidata_results = query_wikidata()
-    #wikidata_df = wikidata_dataframe(wikidata_results)
+    wikidata_results = query_wikidata()
+    wikidata_df = wikidata_dataframe(wikidata_results)
     #merge_dfs(wikidata_df)
 
-    #portals_results = query_portals()
-    #portals_df = portals_dataframe(portals_results)
+    portals_results = query_portals()
+    portals_df = portals_dataframe(portals_results)
     #merge_dfs(portals_df)  
 
 #CLI: dagit -f bin\pipelines\apis_pipeline.py
