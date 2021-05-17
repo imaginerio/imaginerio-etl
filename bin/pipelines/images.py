daf22e0c1db97f3c1abe71e77108651b4c82fa88
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2021-05-17 20:09:26 -0300

    Change conda for WSL2, improve image solids and pipeline

--- bin/pipelines/images.py
+++ bin/pipelines/images.py
@@ -4,11 +4,13 @@ from bin.solids.images import *
 from bin.solids.utils import df_csv_io_manager, root_input, merge_dfs
 
 
-@dg.pipeline(mode_defs=[dg.ModeDefinition(resource_defs={"pandas_csv": df_csv_io_manager, "metatada_root":root_input, "camera_root":root_input})])
+@dg.pipeline(mode_defs=[dg.ModeDefinition(resource_defs={"pandas_csv": df_csv_io_manager, "metadata_root":root_input, "camera_root":root_input})])
 def images_pipeline():
-    image_list = file_picker()
-    file_dispatcher(image_list)
-    images_df = create_images_df(image_list)
-    merge_dfs(images_df)
+    geolocated, backlog = file_picker()
+    to_tag = file_dispatcher(geolocated, backlog)
+    images_df = create_images_df(geolocated, backlog)
+    merge_dfs(df=images_df)
+    # write_metadata(files=to_tag)
+    # upload_to_cloud()
 
 
--- bin/solids/images.py
+++ bin/solids/images.py
@@ -5,8 +5,6 @@ import pandas as pd
 import dagster as dg
 from PIL import Image as PILImage
 
-# from bin.utils import root_input
-
 
 class Image:
 
@@ -19,9 +17,10 @@ class Image:
         self.id = str(os.path.split(self.path)[1].split(".")[0])
 
 
-@dg.solid
-def file_picker(_):
+@dg.solid(input_defs=[dg.InputDefinition("camera", root_manager_key="camera_root")])
+def file_picker(context):
     source = context.solid_config
+    has_kml = list(camera["id"])
     image_list = [
         Image(os.path.join(root, name))
         for root, dirs, files in os.walk(source)
@@ -31,47 +30,50 @@ def file_picker(_):
         and not name.endswith(("v.tif"))
         and not name.endswith("a.tif")
     ]
-    return image_list
+    context.log.info(f"Globbed {len(image_list)} images")
+
+    geolocated = [img for img in image_list if img.id in has_kml]
+    backlog = [img for img in image_list if img.id not in has_kml]
+
+    yield dg.Output(geolocated, "geolocated")
+    yield dg.Output(backlog, "backlog")
 
 
-@dg.solid(input_defs=[dg.InputDefinition("camera", root_manager_key="camera_root")])
-def file_dispatcher(context, image_list, camera):
+@dg.solid
+def file_dispatcher(context, geolocated, backlog):
 
-    geolocated = list(camera["id"])
-    TIFF = context.solid_config["TIFF"]
-    JPEG_HD = context.solid_config["JPEG_HD"]
-    JPEG_SD = context.solid_config["JPEG_SD"]
+    TIFF = context.solid_config["tiff"]
+    JPEG_HD = context.solid_config["jpeg_hd"]
+    JPEG_SD = context.solid_config["jpeg_sd"]
+    IMG_BACKLOG = context.solid_config["backlog"]
 
-    for infile in image_list:
+    def handle_geolocated(infiles):
+        for infile in infiles:
+            if not os.path.exists(os.path.join(TIFF, infile.tif)):
+                shutil.copy2(infile.path, TIFF)
 
-        if not os.path.exists(os.path.join(TIFF, infile.tif)):
-            shutil.copy2(infile.path, TIFF)
-        else:
-            print(f"{infile.tif} already copied")
-
-        if infile.id in geolocated:
-            hdout = os.path.join("src", JPEG_HD, infile.jpg)
-            sdout = os.path.join("src", JPEG_SD, infile.jpg)
+            hdout = os.path.join(JPEG_HD, infile.jpg)
+            sdout = os.path.join(JPEG_SD, infile.jpg)
             size = (1000, 1000)
+
             if not os.path.exists(hdout):
                 try:
                     with PILImage.open(os.path.join(TIFF, infile.tif)) as im:
                         im.save(hdout)
                 except OSError:
-                    print("cannot convert", infile.tif)
-            else:
-                print("HD version already exists")
+                    context.log.info(f"Cannot convert {infile.tif}")
+
             if not os.path.exists(sdout):
                 try:
                     with PILImage.open(os.path.join(TIFF, infile.tif)) as im:
                         im.thumbnail(size)
                         im.save(sdout)
                 except OSError:
-                    print("cannot create thumbnail for", infile.tif)
-            else:
-                print("SD version already exists")
-        else:
-            backlog = os.path.join("src", IMG_BACKLOG, infile.jpg)
+                    context.log.info(f"Cannot create thumbnail for {infile.tif}")
+  
+    def handle_backlog(infiles):
+        for infile in infiles:
+            backlog = os.path.join(IMG_BACKLOG, infile.jpg)
             size = (1000, 1000)
             if not os.path.exists(backlog):
                 try:
@@ -79,19 +81,115 @@ def file_dispatcher(context, image_list, camera):
                         im.thumbnail(size)
                         im.save(backlog)
                 except OSError:
-                    print("cannot create backlog thumbnail for", infile.tif)
-            else:
-                print("Backlog version already exists")
+                    print(f"Cannot create backlog thumbnail for {infile.tif}")
+
+    handle_geolocated(geolocated)
+    handle_backlog(backlog)
+
+    to_tag = [
+        os.path.join(JPEG_HD, file)
+        for file in os.listdir(JPEG_HD)
+        if not os.path.exists(os.path.join(JPEG_HD, f"{file}_original"))
+    ]
+
+    return to_tag
+
+@dg.solid(output_defs=[dg.OutputDefinition(io_manager_key="pandas_csv", name="images")])
+def create_images_df(context, geolocated, backlog)
+    """Creates a dataframe with every image available and links to full size and thumbnail"""
+    
+    hd = context.solid_config
+    sd = hd + "sd/"
+    dicts = []
+
+    for img in geolocated:
+        img_dict = {
+            id: img.id
+            img_hd: os.path.join(hd, img.jpg)
+            img_sd: os.path.join(sd, img.jpg)
+        }
+        dicts.append(img_dict)
+    
+    for img in backlog:
+        img_dict = {
+            id: img.id
+            img_hd: np.Nan
+            img_sd: os.path.join(sd, img.jpg)
+        }
+        dicts.append(img_dict)
+
+    images_df = pd.DataFrame(data=dicts)
+    images_df.sort_values(by='id')
+    return images_df
 
 
-@dg.solid
-def create_images_df(_, image_list):
-    return
+@dg.solid(input_defs=[dg.InputDefinition("metadata", root_manager_key:"metadata_root"), dg.InputDefinition("ok", dg.Nothing)])
+def write_metadata(_, metadata, files):
 
+    metadata.fillna(value="", inplace=True)
+    metadata.set_index("id", inplace=True)
 
-@dg.solid
-def write_metadata(_, jpeg_hd):
-    return jpeg_hd
+    for i, item in enumerate(files):
+        if item.endswith(".jpg"):
+            basename = os.path.split(item)[1]
+            name = basename.split(".")[0]
+            try:
+                if metadata.loc[name, "date_accuracy"] == "circa":
+                    datecreated = (
+                        metadata.loc[name, "start_date"].strftime("%Y")
+                        + "/"
+                        + metadata.loc[name, "end_date"].strftime("%Y")
+                    )
+                elif metadata.loc[name, "date_accuracy"] == "year":
+                    datecreated = metadata.loc[name, "date"].strftime("%Y")
+                elif metadata.loc[name, "date_accuracy"] == "month":
+                    datecreated = metadata.loc[name, "date"].strftime("%Y-%m")
+                elif metadata.loc[name, "date_accuracy"] == "day":
+                    datecreated = metadata.loc[name, "date"].strftime("%Y-%m-%d")
+            except AttributeError:
+                print(f"Review {basename} date")
+            byline = metadata.loc[name, "creator"]
+            headline = metadata.loc[name, "title"]
+            caption = metadata.loc[name, "description"]
+            objecttype = metadata.loc[name, "type"]
+            dimensions = (
+                f'{metadata.loc[name, "image_width"]}cm x {metadata.loc[name, "image_height"]}cm'
+            )
+            keywords = metadata.loc[name, "wikidata_depict"].split("||")
+            latitude = metadata.loc[name, "lat"]
+            longitude = metadata.loc[name, "lng"]
+            altitude = metadata.loc[name, "height"]
+            imgdirection = metadata.loc[name, "heading"]
+
+            params = [
+                "-IPTC:Source=Instituto Moreira Salles/IMS",
+                "-IPTC:CopyrightNotice=This image is in the Public Domain.",
+                "-IPTC:City=Rio de Janeiro",
+                "-IPTC:Province-State=RJ",
+                "-IPTC:Country-PrimaryLocationName=Brasil",
+                "-GPSLatitudeRef=S",
+                "-GPSLongitudeRef=W",
+                "-GPSAltitudeRef=0",
+                "-GPSImgDirectionRef=T",
+                f"-IPTC:By-line={byline}",
+                f"-IPTC:ObjectName={name}",
+                f"-IPTC:Headline={headline}",
+                f"-IPTC:Caption-Abstract={caption}",
+                f"-IPTC:ObjectTypeReference={objecttype}",
+                f"-IPTC:Keywords={keywords}",
+                f"-GPSLatitude={latitude}",
+                f"-GPSLongitude={longitude}",
+                f"-GPSAltitude={altitude}",
+                f"-GPSImgDirection={imgdirection}",
+            ]
+            with exiftool.ExifTool(executable_=context.solid_config) as et:
+                for param in params:
+                    param = param.encode(encoding="utf-8")
+                    dest = item.encode(encoding="utf-8")
+                    et.execute(param, dest)
+            print(
+                f"{basename}\n{metadata.loc[name, 'date']}\nTagged {i+1} of {len(files)} images"
+            )
 
 
 @dg.solid
