f19214e156c8bf2f30489563710d45084d6a99b6
PitangaVigand <pitangavigand@gmail.com>
2021-05-18 12:59:26 -0300

    Update catalog.py with input_root and marge

---
+++ bin/repositories/situated_views.py
@@ -0,0 +1,16 @@
+  
+import dagster as dg
+from bin.pipelines.catalog import catalog_pipeline
+from bin.pipelines.images import images_pipeline
+from bin.schedules.daily import daily
+
+
+@dg.repository
+def situated_views():
+    return {
+        "pipelines": {
+            "catalog_pipeline": lambda: catalog_pipeline,
+            "images_pipeline": lambda: images_pipeline,
+        },
+        "schedules": {"daily": lambda: daily},
+    }
---
+++ bin/solids/utils.py
@@ -0,0 +1,75 @@
+#import geopandas as gpd
+import os
+from typing import Any
+from xml.etree import ElementTree
+
+import dagster as dg
+import geojson
+import numpy as np
+import pandas as pd
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
+
+
+class PandasCsvIOManager(dg.IOManager):
+    def load_input(self, context):
+        file_path = os.path.join("data-out", context.upstream_output.name)
+        return pd.read_csv(file_path + ".csv")
+    
+    def handle_output(self, context, obj):
+        file_path = os.path.join("data-out", context.name)
+        obj.to_csv(file_path + ".csv", index=False)
+
+        yield dg.AssetMaterialization(asset_key = dg.AssetKey(file_path), description = "saved csv")
+
+@dg.io_manager
+def df_csv_io_manager(init_context):
+    return PandasCsvIOManager()
+
+
+class GeojsonIOManager(dg.IOManager):
+    def load_input(self, context):
+        file_path = context.upstream_output.name
+        return geojson.loads(file_path + ".geojson")
+    
+    def handle_output(self, context, feature_collection):
+        file_path = os.path.join("data-out", context.name,"geojson")
+        with open(file_path, "w", encoding="utf-8") as f:
+            geojson.dump(feature_collection, f, ensure_ascii=False, indent=4)        
+
+        yield dg.AssetMaterialization(asset_key = dg.AssetKey(file_path), description = "saved geojson")
+
+@dg.io_manager
+def geojson_io_manager(init_context):
+    return GeojsonIOManager()
+
+@dg.solid
+def rename_column(context,df,dic):
+    df = df.rename(columns = dic)
+    return df
+
+@dg.solid(
+    input_defs=[dg.InputDefinition("metadata", root_manager_key="metadata_root")],
+    output_defs=[dg.OutputDefinition(io_manager_key="pandas_csv", name="metadata")]    
+)
+def merge_dfs(_,metadata,df):
+    metadata = df.combine_first(metadata)    
+    return metadata
+
+@dg.root_input_manager
+def root_input(context):
+    return pd.read_csv(context.config['path'])
+
+@dg.root_input_manager
+def root_input_xml(context):
+    path = context.config['path']
+    with open(path, encoding="utf8") as f:
+        tree = ElementTree.parse(f)
+    root = tree.getroot()
+    return root       
+
+@dg.solid(required_resource_keys={'slack'})
+def slack_solid(context):
+    context.resources.slack.chat_postMessage(channel='#tutoriais-e-links', text=':wave: teste!')
+
