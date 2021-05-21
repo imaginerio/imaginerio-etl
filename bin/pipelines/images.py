2c6258f07378f359cda7993f9d940431bb2c2dd8
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2021-05-21 16:33:35 -0300

    Create write_metadata solid in images pipeline

--- bin/pipelines/images.py
+++ bin/pipelines/images.py
@@ -6,11 +6,11 @@ from bin.solids.utils import df_csv_io_manager, root_input, merge_dfs
 
 @dg.pipeline(mode_defs=[dg.ModeDefinition(resource_defs={"pandas_csv": df_csv_io_manager, "metadata_root":root_input, "camera_root":root_input})])
 def images_pipeline():
-    geolocated, backlog = file_picker()
-    to_tag = file_dispatcher(geolocated, backlog)
-    images_df = create_images_df(geolocated, backlog)
+    files = file_picker()
+    to_tag = file_dispatcher(files)
+    images_df = create_images_df(files)
     merge_dfs(df=images_df)
-    # write_metadata(files=to_tag)
+    write_metadata(files_to_tag=to_tag)
     # upload_to_cloud()
 
 
