d792ad3e09f66c14cf55cbdd554d0f2a46875fd0
unknown <mechler.cintia@gmail.com>
2021-05-24 15:56:21 -0300

    Update apis files

--- bin/pipelines/apis_pipeline.py
+++ bin/pipelines/apis_pipeline.py
@@ -1,29 +1,26 @@
 import os
 import dagster as dg
+from dagster_slack import slack_resource
 
-from bin.solids.utils import df_csv_io_manager,query_api
-from bin.solids.apis import query_omeka, create_dataframes
-
-
-#query_omeka = configured(query_api, name="query_omeka")({"env": OMEKA_API"})
-#query_wikidata = configured(query_api, name="aquery_wikidata ")({"env": "WIKIDATA_API"})
-#query_portals= configured(query_api, name="aquery_wikidata ")({"env": os.environ["WIKIDATA_API"]})
-
-#query_omeka = load_omeka.alias('omeka')
-#query_wikidata = query_api.alias('wikidata')
-#query_portals = query_api.alias('portals')
+from bin.solids.utils import df_csv_io_manager, merge_dfs, root_input, slack_solid
+from bin.solids.apis import portals_dataframe, query_portals, query_wikidata, query_omeka, omeka_dataframe, wikidata_dataframe
 
 
-@dg.pipeline(mode_defs =[dg.ModeDefinition(resource_defs={"pandas_csv":df_csv_io_manager})])
+@dg.pipeline(mode_defs =[dg.ModeDefinition(resource_defs={"pandas_csv":df_csv_io_manager, "metadata_root":root_input, "slack": slack_resource})])
 def apis(): 
+    #slack_solid()
 
-    results = query_omeka()
-    create_dataframes(results)
+    omeka_results = query_omeka()
+    omeka_df = omeka_dataframe(omeka_results)
+    merge_dfs(df=omeka_df)   
 
-    
+    wikidata_results = query_wikidata()
+    wikidata_df = wikidata_dataframe(wikidata_results)
+    merge_dfs(df=wikidata_df)
 
+    portals_results = query_portals()
+    portals_df = portals_dataframe(portals_results)
+    merge_dfs(df=portals_df)  
 
-
-
-#CLI: dagit -f bin\pipelines\apis_pipeline.py
+#CLI: dagit -f bin/pipelines/apis_pipeline.py
     
