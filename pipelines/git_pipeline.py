e5b028fbcc18b2dbd39f59bb0bfded5689e3ac4c
PitangaVigand <pitangavigand@gmail.com>
2021-07-09 19:44:00 -0300

    :hammer:Change sensor to schedule for automatic push

--- pipelines/git_pipeline.py
+++ pipelines/git_pipeline.py
@@ -12,28 +12,38 @@ def git_pipeline():
 
 
 ################   SENSORS   ##################
-@dg.sensor(
-    pipeline_name="git_pipeline",
-    solid_selection=["push_new_data"],
-    minimum_interval_seconds=300,
-)
-def trigger_git_push(context):
-    events = context.instance.events_for_asset_key(
-        dg.AssetKey("metadata"),
-        after_cursor=context.last_run_key,
-        ascending=False,
-        limit=1,
-    )
-    if events:
-        record_id, event = events[0]  # take the most recent materialization
-        yield dg.RunRequest(
-            run_key=str(record_id),
-            run_config={},
-            tags={"source_pipeline": event.pipeline_name},
-        )
+# @dg.sensor(
+#     pipeline_name="git_pipeline",
+#     solid_selection=["push_new_data"],
+#     minimum_interval_seconds=300,
+# )
+# def trigger_git_push(context):
+#     events = context.instance.events_for_asset_key(
+#         dg.AssetKey("metadata"),
+#         after_cursor=context.last_run_key,
+#         ascending=False,
+#         limit=1,
+#     )
+#     if events:
+#         record_id, event = events[0]  # take the most recent materialization
+#         yield dg.RunRequest(
+#             run_key=str(record_id),
+#             run_config={},
+#             tags={"source_pipeline": event.pipeline_name},
+#         )
 
 
 ################   SCHEDULES   ##################
+
+
+@dg.schedule(
+    cron_schedule="0 0 * * *",
+    pipeline_name="git_pipeline",
+    solid_selection=["push_new_data"],
+    execution_timezone="America/Sao_Paulo",
+)
+def push_new_data_daily():
+    return {}
 
 
 @dg.schedule(
@@ -42,7 +52,7 @@ def trigger_git_push(context):
     solid_selection=["pull_new_data"],
     execution_timezone="America/Sao_Paulo",
 )
-def pull_new_data_weekly():
+def pull_new_data_hour():
     return {}
 
 
