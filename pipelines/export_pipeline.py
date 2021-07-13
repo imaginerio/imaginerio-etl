b06c8087270d2f84f9cffdafebc52ad1de14ba25
mechlerc <mechler.cintia@gmail.com>
2021-07-13 19:28:46 -0300

    :hammer: Add solids to create pie and bar chart and load smapshot.csv using root_input

--- pipelines/export_pipeline.py
+++ pipelines/export_pipeline.py
@@ -10,6 +10,7 @@ preset = {
     "resources": {
         "metadata_root": {"config": {"env": "METADATA"}},
         "jstor_root": {"config": {"env": "JSTOR"}},
+        "smapshot_root":{"config":{"env":"SMAPSHOT"}}
     },
 }
 
@@ -22,6 +23,7 @@ preset = {
                 "pandas_csv": df_csv_io_manager,
                 "jstor_root": root_input_csv,
                 "metadata_root": root_input_csv,
+                "smapshot_root": root_input_csv,
             },
         )
     ],
@@ -42,9 +44,15 @@ def export_pipeline():
     omeka_df = organize_columns_to_omeka(export_df)
     omeka_df = import_omeka_dataframe(df=omeka_df)
 
-    # import WIKIDATA
+    # Import WIKIDATA
     wikidata_df = make_df_to_wikidata(export_df)
     wikidata_df = organise_creator(quickstate=wikidata_df)
+
+    # index.html
+    #graph_html = format_values_chart(export_df)
+    #plot_hbar = create_hbar(graph_html[0])
+    #plot_pie = create_pie(graph_html[1])
+    #graph_html = export_html(plot_hbar,plot_pie)
 
 
 ################   SENSORS   ##################
