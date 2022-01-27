9a713d280e6902b8b2ef3b3cf515d34db6ee0e28
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2022-01-27 17:51:01 -0300

    :art::recycle: Refactor image handling as OOP

--- ops/images.py
+++ ops/images.py
@@ -21,176 +21,216 @@ load_dotenv(override=True)
 
 
 class Image:
+    def __init__(self, original_path, has_kml, catalog):
+        self.__original_path = original_path
+        self.__id = os.path.split(self.__original_path)[1].split(".")[0]
+        self.__jpg = self.__id + ".jpg"
+        self.__tif = self.__id + ".tif"
+        self.__is_geolocated = self.__id in has_kml
+        self.__in_catalog = self.__id in catalog
+        self.__metadata = None
+        self.__on_cloud = None
 
-    """
-    Original image path. Attributes return id, jpg or tif extension.
-    """
+    @property
+    def id(self):
+        return self.__id
 
-    def __init__(self, path):
-        self.path = path
-        self.jpg = str(os.path.split(self.path)[1].split(".")[0] + ".jpg")
-        self.tif = str(os.path.split(self.path)[1])
-        self.id = str(os.path.split(self.path)[1].split(".")[0])
+    @property
+    def jpg(self):
+        return self.__jpg
+
+    @property
+    def tif(self):
+        return self.__tif
+
+    @property
+    def is_geolocated(self):
+        return self.__is_geolocated
+
+    # @is_geolocated.setter
+    # def is_geolocated(self, has_kml):
+    #    self.__is_geolocated = self.__id in has_kml
+
+    @property
+    def in_catalog(self):
+        return self.__in_catalog
+
+    # @in_catalog.setter
+    # def in_catalog(self, catalog):
+    #    self.__in_catalog = self.__id in catalog
+
+    @property
+    def to_tif(self):
+        return not os.path.exists(os.path.join(os.environ["TIF"], self.__tif))
+
+    @property
+    def to_jpg(self):
+        return (
+            not os.path.exists(os.path.join(os.environ["JPG"], self.__jpg))
+            and self.__is_geolocated
+        )
+
+    @property
+    def to_backlog(self):
+        return (
+            not os.path.exists(os.path.join(os.environ["BACKLOG"], self.__jpg))
+            and not self.__is_geolocated
+            and self.__in_catalog
+        )
+
+    @property
+    def to_review(self):
+        return (
+            not os.path.exists(os.path.join(os.environ["REVIEW"], self.__jpg))
+            and not self.__in_catalog
+        )
+
+    @property
+    def on_cloud(self):
+        pass
+
+    def copy_to(self, destination):
+        if destination.endswith("tif"):
+            print("Copying {} to {}".format(self.__id, destination))
+            shutil.copy2(self.__original_path, destination)
+        elif destination.endswith("backlog") or destination.endswith("review"):
+            print("Copying {} to {}".format(self.__id, destination))
+            try:
+                with PILImage.open(os.path.join(os.environ["TIF"], self.__tif)) as im:
+                    im.thumbnail((1000, 1000))
+                    im.save(os.path.join(destination, self.__jpg))
+            except OSError:
+                print(f"Cannot convert {self.__tif}")
+        else:
+            print("Copying {} to {}".format(self.__id, destination))
+            try:
+                with PILImage.open(os.path.join(os.environ["TIF"], self.__tif)) as im:
+                    im.save(os.path.join(destination, self.__jpg))
+            except OSError:
+                print(f"Cannot convert {self.__tif}")
+
+    @property
+    def has_embedded_metadata(self):
+        return os.path.exists(
+            os.path.join(os.environ["JPG"], "{}_original".format(self.__jpg))
+        )
+
+    @property
+    def metadata(self):
+        return self.__metadata
+
+    @metadata.setter
+    def metadata(self, metadata):
+        metadata.fillna(value="", inplace=True)
+        metadata["Source ID"] = metadata["Source ID"].str.upper()
+        metadata.set_index("Source ID", inplace=True)
+        item = metadata.loc[self.__id]
+        self.__metadata = [
+            "-IPTC:Source=Instituto Moreira Salles/IMS",
+            "-IPTC:CopyrightNotice=This image is in the Public Domain.",
+            "-IPTC:City=Rio de Janeiro",
+            "-IPTC:Province-State=RJ",
+            "-IPTC:Country-PrimaryLocationName=Brasil",
+            "-GPSLatitudeRef=S",
+            "-GPSLongitudeRef=W",
+            "-GPSAltitudeRef=0",
+            "-GPSImgDirectionRef=T",
+            "-IPTC:DateCreated={}".format(item["Date"]),
+            "-IPTC:By-line={}".format(item["Creator"]),
+            "-IPTC:ObjectName={}".format(self.__id),
+            "-IPTC:Headline={}".format(item["Title"]),
+            "-IPTC:Caption-Abstract={}".format(item["Description (Portuguese)"]),
+            "-IPTC:ObjectTypeReference={}".format(item["Type"]),
+            # "-IPTC:Dimensions={}x{}mm".format(item["Width"], item["Height"]),
+            "-IPTC:Keywords={}".format(",".join(item["Depicts"].split("||"))),
+            "-GPSLatitude={}".format(item["Latitude"]),
+            "-GPSLongitude={}".format(item["Longitude"]),
+            # "-GPSAltitude={}".format(item["Altitude"]),
+            # "-GPSImgDirection={}".format(item["Bearing"]),
+        ]
+
+    def embed_metadata(self):
+        with ExifTool(executable_=os.environ["EXIFTOOL"]) as et:
+            for param in self.__metadata:
+                param = param.encode(encoding="utf-8")
+                dest = os.path.join(os.environ["JPG"], self.__jpg).encode(
+                    encoding="utf-8"
+                )
+                et.execute(param, dest)
+
+    def upload_to_cloud(self):
+        pass
 
 
 @op(
     config_schema=dg.StringSource,
     ins={"metadata": In(root_manager_key="metadata_root")},
-    out=Out(dagster_type=dict),
+    out=Out(dagster_type=list),
 )
 def file_picker(context, metadata: dp.DataFrame):
     """
-    Walks directory tree and glob relevant files.
-    Returns dictionary with geolocated, backlog
-    and files for review
+    Walks directory tree and glob relevant files,
+    instantiating Image objects for each
     """
+
     source = context.solid_config
+
     metadata["Source ID"] = metadata["Source ID"].str.upper()
     has_kml = list(metadata.loc[metadata["Latitude"].notna(), "Source ID"])
     catalog = list(metadata["Source ID"])
-    image_list = [
-        Image(os.path.join(root, name))
-        for root, dirs, files in os.walk(source)
+    images = [
+        Image(os.path.join(root, name), has_kml, catalog)
+        for root, _, files in os.walk(source)
         for name in files
         if "FINALIZADAS" in root
         and name.endswith((".tif"))
-        and not re.search("[a-z]\.tif$", name)
+        and not re.search("[av]\.tif$", name)
     ]
 
-    geolocated = [img for img in image_list if img.id.upper() in has_kml]
-    backlog = [
-        img
-        for img in image_list
-        if img.id.upper() in catalog and img.id.upper() not in has_kml
-    ]
-    review = [img for img in image_list if img.id.upper() not in catalog]
-
-    context.log.info(
-        f"Geolocated: {len(set([img.id for img in geolocated]))} images; Backlog: {len(set([img.id for img in backlog]))} images"
-    )
-
-    return {"geolocated": geolocated, "backlog": backlog, "review": review}
+    return images
 
 
 @op(
     config_schema={
-        "tiff": dg.StringSource,
-        "jpeg_hd": dg.StringSource,
-        "jpeg_sd": dg.StringSource,
+        "tif": dg.StringSource,
+        "jpg": dg.StringSource,
         "backlog": dg.StringSource,
         "review": dg.StringSource,
     }
 )
-def file_dispatcher(context, files: dict):
+def file_dispatcher(context, images: list):
     """
-    Copy failsafe TIFFs, generate high and low
-    res JPEGs, and separate files for review
+    Copy failsafe TIFs, convert geolocated images
+    to JPG, and separate files for review and backlog
     """
 
-    TIFF = context.solid_config["tiff"]
-    JPEG_HD = context.solid_config["jpeg_hd"]
-    JPEG_SD = context.solid_config["jpeg_sd"]
-    IMG_BACKLOG = context.solid_config["backlog"]
+    TIF = context.solid_config["tif"]
+    JPG = context.solid_config["jpg"]
+    BACKLOG = context.solid_config["backlog"]
     REVIEW = context.solid_config["review"]
 
-    def handle_geolocated(infiles):
-        for infile in tqdm(infiles, "Handling geolocated..."):
-            if not os.path.exists(os.path.join(TIFF, infile.tif)):
-                shutil.copy2(infile.path, TIFF)
+    for image in images:
 
-            hd_path = os.path.join(JPEG_HD, infile.jpg)
-            sd_path = os.path.join(JPEG_SD, infile.jpg)
-            review_path = os.path.join(REVIEW, infile.jpg)
-            size = (1000, 1000)
+        if image.to_tif:
+            image.copy_to(TIF)
 
-            if not os.path.exists(hd_path):
-                try:
-                    with PILImage.open(os.path.join(TIFF, infile.tif)) as im:
-                        im.save(hd_path)
-                except OSError:
-                    context.log.info(f"Cannot convert {infile.tif}")
+        if image.to_jpg:
+            image.copy_to(JPG)
 
-            if not os.path.exists(sd_path):
-                try:
-                    with PILImage.open(os.path.join(TIFF, infile.tif)) as im:
-                        im.thumbnail(size)
-                        im.save(sd_path)
-                except OSError:
-                    context.log.info(f"Cannot create thumbnail for {infile.tif}")
+        if image.to_backlog:
+            image.copy_to(BACKLOG)
 
-            if os.path.exists(review_path):
-                os.remove(review_path)
+        if image.to_review:
+            image.copy_to(REVIEW)
 
-    def handle_backlog(infiles):
-        for infile in tqdm(infiles, "Handling backlog..."):
-            if not os.path.exists(os.path.join(TIFF, infile.tif)):
-                shutil.copy2(infile.path, TIFF)
-            backlog_path = os.path.join(IMG_BACKLOG, infile.jpg)
-            review_path = os.path.join(REVIEW, infile.jpg)
-            size = (1000, 1000)
-            if not os.path.exists(backlog_path):
-                try:
-                    with PILImage.open(os.path.join(TIFF, infile.tif)) as im:
-                        im.thumbnail(size)
-                        im.save(backlog_path)
-                except OSError:
-                    context.log.info(
-                        f"Cannot create backlog thumbnail for {infile.tif}"
-                    )
-            if os.path.exists(review_path):
-                os.remove(review_path)
-        current_backlog = os.listdir(IMG_BACKLOG)
-        for image in current_backlog:
-            if image in [img.jpg for img in files["geolocated"]]:
-                if not os.path.exists(os.path.join(JPEG_SD, image)):
-                    os.rename(
-                        os.path.join(IMG_BACKLOG, image), os.path.join(JPEG_SD, image)
-                    )
-                else:
-                    os.remove(os.path.join(IMG_BACKLOG, image))
-            else:
-                continue
-
-    def handle_review(infiles):
-        # current_hd = [os.path.join(JPEG_HD, img) for img in os.listdir(JPEG_HD) if img.split(".")[0] in files["review"]]
-        for infile in tqdm(infiles, "Handling review..."):
-            review_path = os.path.join(REVIEW, infile.jpg)
-            tiff_path = os.path.join(TIFF, infile.tif)
-            if not os.path.exists(review_path):
-                try:
-                    with PILImage.open(infile.path) as im:
-                        im.save(review_path)
-                except OSError:
-                    context.log.info(f"Cannot convert {infile.tif}")
-            else:
-                continue
-            if os.path.exists(tiff_path):
-                os.remove(tiff_path)
-
-    handle_geolocated(files["geolocated"])
-    handle_backlog(files["backlog"])
-    handle_review(files["review"])
-
-    to_tag = [
-        os.path.join(JPEG_HD, file)
-        for file in os.listdir(JPEG_HD)
-        if not os.path.exists(os.path.join(JPEG_HD, f"{file}_original"))
-        and not file.endswith("_original")
-    ]
-    # print("to_tag", type(to_tag))
-    if to_tag:
-        context.log.info(
-            f"Passed {len(to_tag)} images to be tagged. Path example: {to_tag[0]}"
-        )
-    return to_tag
+    return images
 
 
 @op(
     config_schema=dg.StringSource,
     out={"images": Out(io_manager_key="pandas_csv", dagster_type=pd.DataFrame)},
 )
-def create_images_df(context, files: dict):
-
+def create_images_df(context, images: list):
     """
     Creates a dataframe with every image available and links to full size and thumbnail
     """
@@ -198,20 +238,15 @@ def create_images_df(context, files: dict):
     prefix = context.solid_config
     dicts = []
 
-    for img in files["geolocated"]:
+    for img in images:
         img_dict = {
             "Source ID": img.id,
-            "Media URL": os.path.join(
-                prefix, img.id, "full", "max", "0", "default.jpg"
-            ),
         }
-        dicts.append(img_dict)
-
-    for img in files["backlog"]:
-        img_dict = {
-            "Source ID": img.id,
-            "Media URL": np.nan,
-        }
+        img_dict["Media URL"] = (
+            os.path.join(prefix, img.id, "full", "max", "0", "default.jpg")
+            if img.is_geolocated
+            else np.nan
+        )
         dicts.append(img_dict)
 
     images_df = pd.DataFrame(data=dicts)
@@ -226,77 +261,41 @@ def create_images_df(context, files: dict):
     config_schema=dg.StringSource,
     ins={"metadata": In(root_manager_key="metadata_root")},
 )
-def write_metadata(context, metadata: dp.DataFrame, to_tag):
+def embed_metadata(context, metadata: dp.DataFrame, images):
     """
     Write available metadata, including GPS tags,
-    to high-res JPEGs
+    to high-res JPGs
     """
-    metadata.fillna(value="", inplace=True)
-    metadata["Source ID"] = metadata["Source ID"].str.upper()
-    metadata.set_index("Source ID", inplace=True)
 
-    for item in tqdm(to_tag, "Embedding metadata in files..."):
-        if item.endswith(".jpg"):
-            basename = os.path.split(item)[1]
-            name = basename.split(".")[0]
-            date = metadata.loc[name.upper(), "Date"]
-            byline = metadata.loc[name.upper(), "Creator"]
-            headline = metadata.loc[name.upper(), "Title"]
-            caption = metadata.loc[name.upper(), "Description (Portuguese)"]
-            objecttype = metadata.loc[name.upper(), "Type"]
-            # dimensions = f'{metadata.loc[name.upper(), "image_width"]}cm x {metadata.loc[name.upper(), "image_height"]}cm'
-            keywords = metadata.loc[name.upper(), "Depicts"].split("||")
-            latitude = metadata.loc[name.upper(), "Latitude"]
-            longitude = metadata.loc[name.upper(), "Longitude"]
-            # altitude = metadata.loc[name.upper(), "Altitude"]
-            # imgdirection = metadata.loc[name.upper(), "heading"]
+    for image in images:
+        if image.is_geolocated and not image.has_embedded_metadata:
+            image.get_metadata = metadata
+            image.embed_metadata
 
-            params = [
-                "-IPTC:Source=Instituto Moreira Salles/IMS",
-                "-IPTC:CopyrightNotice=This image is in the Public Domain.",
-                "-IPTC:City=Rio de Janeiro",
-                "-IPTC:Province-State=RJ",
-                "-IPTC:Country-PrimaryLocationName=Brasil",
-                "-GPSLatitudeRef=S",
-                "-GPSLongitudeRef=W",
-                "-GPSAltitudeRef=0",
-                "-GPSImgDirectionRef=T",
-                f"-IPTC:DateCreated={date}",
-                f"-IPTC:By-line={byline}",
-                f"-IPTC:ObjectName={name}",
-                f"-IPTC:Headline={headline}",
-                f"-IPTC:Caption-Abstract={caption}",
-                f"-IPTC:ObjectTypeReference={objecttype}",
-                # f"-IPTC:Dimensions={dimensions}",
-                f"-IPTC:Keywords={keywords}",
-                f"-GPSLatitude={latitude}",
-                f"-GPSLongitude={longitude}",
-                # f"-GPSAltitude={altitude}",
-                # f"-GPSImgDirection={imgdirection}",
-            ]
-            with ExifTool(executable_=context.solid_config) as et:
-                for param in params:
-                    param = param.encode(encoding="utf-8")
-                    dest = item.encode(encoding="utf-8")
-                    et.execute(param, dest)
-    to_upload = to_tag
-    return to_upload
+    return images
 
 
 @op
-def upload_to_cloud(context, to_upload):
+def upload_to_cloud(context, images: list):
+    """
+    Uploads finished JPGs to AWS S3
+    """
 
-    S3 = boto3.client("s3")
-    BUCKET = "imaginerio-images"
-    for image_path in tqdm(to_upload, "Uploading files..."):
-        id = os.path.basename(image_path).split(".")[0]
-        key_name = "iiif/{0}/full/max/0/default.jpg".format(id)
-        try:
-            S3.upload_file(
-                image_path,
-                BUCKET,
-                key_name,
-                ExtraArgs={"ACL": "public-read", "ContentType": "image/jpeg"},
-            )
-        except:
-            print("Couldn't upload image {0}, skipping".format(id))
+    for image in images:
+        if not image.on_cloud:
+            image.upload_to_cloud
+
+    # S3 = boto3.client("s3")
+    # BUCKET = "imaginerio-images"
+    # for image_path in tqdm(to_upload, "Uploading files..."):
+    #     id = os.path.basename(image_path).split(".")[0]
+    #     key_name = "iiif/{0}/full/max/0/default.jpg".format(id)
+    #     try:
+    #         S3.upload_file(
+    #             image_path,
+    #             BUCKET,
+    #             key_name,
+    #             ExtraArgs={"ACL": "public-read", "ContentType": "image/jpeg"},
+    #         )
+    #     except:
+    #         print("Couldn't upload image {0}, skipping".format(id))
