a48275460e1afa8784c4518507b3430472774fbe
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2024-02-29 15:45:11 -0300

    Ditch .env.example, will use gh env vars

--- imaginerio-etl/config.py
+++ imaginerio-etl/config.py
@@ -1,18 +1,13 @@
 import os
 
-from dotenv import load_dotenv
-
-# load_dotenv(override=True)
-
-IMS_METADATA = os.getenv("IMS_METADATA")
-IMS2JSTOR = os.getenv("IMS2JSTOR")
-REVIEW = os.getenv("REVIEW")
-# JSTOR = os.getenv("JSTOR")
-# VOCABULARY = os.getenv("VOCABULARY")
-# BUCKET = os.getenv("BUCKET")
-# CLOUDFRONT = os.getenv("CLOUDFRONT")
-DISTRIBUTION_ID = os.getenv("DISTRIBUTION_ID")
-
 JSTOR = "data/input/jstor.xls"
 VOCABULARY = "data/input/vocabulary.xls"
 CLOUDFRONT = "https://iiif.imaginerio.org/iiif"
+BUCKET = "https://imaginerio-images.s3.us-east-1.amazonaws.com/"
+DISTRIBUTION_ID = os.getenv("DISTRIBUTION_ID")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_KEY")
+
+# IMS_METADATA = os.getenv("IMS_METADATA")
+# IMS2JSTOR = os.getenv("IMS2JSTOR")
+# REVIEW = os.getenv("REVIEW")
