191b1eef4aafe8e4b1bce5a624f0df0b6f1abebf
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2024-02-29 17:47:54 -0300

    Use gh env vars

--- imaginerio-etl/config.py
+++ imaginerio-etl/config.py
@@ -4,9 +4,9 @@ JSTOR = "data/input/jstor.xls"
 VOCABULARY = "data/input/vocabulary.xls"
 CLOUDFRONT = "https://iiif.imaginerio.org/iiif"
 BUCKET = "https://imaginerio-images.s3.us-east-1.amazonaws.com/"
-DISTRIBUTION_ID = os.getenv("DISTRIBUTION_ID")
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_KEY")
+# DISTRIBUTION_ID = os.getenv("DISTRIBUTION_ID")
+# AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
 # IMS_METADATA = os.getenv("IMS_METADATA")
 # IMS2JSTOR = os.getenv("IMS2JSTOR")
