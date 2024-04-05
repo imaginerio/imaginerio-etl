ac4cb85eea76c44c0d4c5cebccff75cfaa3f61b4
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2024-04-05 14:26:34 -0300

    Add ArcGIS env variables

--- imaginerio-etl/config.py
+++ imaginerio-etl/config.py
@@ -2,10 +2,16 @@ import os
 
 JSTOR = "data/input/jstor.xls"
 VOCABULARY = "data/input/vocabulary.xls"
+KMLS_IN = "data/input/kmls"
+KMLS_OUT = "data/output/kmls"
 GEOJSON = "data/output/viewcones.geojson"
 CLOUDFRONT = "https://iiif.imaginerio.org/iiif"
 BUCKET = "https://imaginerio-images.s3.us-east-1.amazonaws.com/"
 DISTRIBUTION_ID = os.getenv("DISTRIBUTION_ID")
+ARCGIS_USER = os.getenv("ARCGIS_USER")
+ARCGIS_PASSWORD = os.getenv("ARCGIS_PASSWORD")
+VIEWCONES_LAYER_URL = os.getenv("VIEWCONES_LAYER_URL")
+
 # AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 # AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
