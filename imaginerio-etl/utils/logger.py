0608da1aae208104dd6d5354a4199ed61a574adc
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2024-04-09 10:39:20 -0300

    Silence boto3 loggers

--- imaginerio-etl/utils/logger.py
+++ imaginerio-etl/utils/logger.py
@@ -102,4 +102,10 @@ logging.config.dictConfig(
     }
 )
 
+logging.getLogger("boto3").setLevel(logging.CRITICAL)
+logging.getLogger("botocore").setLevel(logging.CRITICAL)
+logging.getLogger("nose").setLevel(logging.CRITICAL)
+logging.getLogger("s3transfer").setLevel(logging.CRITICAL)
+logging.getLogger("urllib3").setLevel(logging.CRITICAL)
+
 logger = logging.getLogger(__name__)
