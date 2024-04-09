2369647aa6ab2d48639a6370b64d4c534ae12171
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2024-04-09 19:38:11 -0300

    Only create items_to_process if there are any

--- imaginerio-etl/utils/compare_data.py
+++ imaginerio-etl/utils/compare_data.py
@@ -3,14 +3,16 @@ import os
 import pandas as pd
 
 current_file = "data/input/jstor.xls"
-current = pd.read_excel(current_file)
+current_data = pd.read_excel(current_file)
 
 new_file = os.path.join("jstor_download", os.listdir("jstor_download")[0])
-new = pd.read_excel(new_file)
+new_data = pd.read_excel(new_file)
 
-comparison = new.compare(current, keep_shape=True)
+comparison = new_data.compare(current_data, keep_shape=True)
 changes = comparison.notna().any(axis=1)
-to_process = new[changes]
+to_process = new_data[changes]
 
 os.replace(new_file, current_file)
-to_process.to_excel("data/input/items_to_process.xls", engine="openpyxl")
+
+if not to_process.loc[to_process["Status[30205]"] == "in imagineRio"].empty:
+    to_process.to_excel("data/input/items_to_process.xls", engine="openpyxl")
