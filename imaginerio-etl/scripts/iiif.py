40514779dd57ab289e86b977f3692bb428ebb591
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2024-04-10 17:24:32 -0300

    Metadata as argument, no args, move summary to helper function

--- imaginerio-etl/scripts/iiif.py
+++ imaginerio-etl/scripts/iiif.py
@@ -1,21 +1,13 @@
-import argparse
-import sys
-
 from ..config import *
 from ..entities.item import Item
-from ..utils.helpers import get_collections, get_metadata, upload_object_to_s3
+from ..utils.helpers import get_collections, get_vocabulary, upload_object_to_s3
 from ..utils.logger import CustomFormatter as cf
 from ..utils.logger import logger
 
-# from math import *
 
-
-def main(args):  # ={"source": ITEMS_TO_PROCESS, "index": "all", "retile": False}
-    metadata, vocabulary = get_metadata(args.source, VOCABULARY, args.index)
-    if metadata.empty:
-        logger.info("There aren't any new or changed items to process. Exiting.")
-        sys.exit(0)
-    collections = get_collections(metadata, args.index)
+def update_manifests(metadata):
+    vocabulary = get_vocabulary(VOCABULARY)
+    collections = get_collections(metadata)
     n_manifests = 0
     errors = []
     no_collection = metadata.loc[metadata["Collection"].isna()].index.to_list()
@@ -27,7 +19,7 @@ def main(args):  # ={"source": ITEMS_TO_PROCESS, "index": "all", "retile": False
         try:
             item = Item(id, row, vocabulary)
             sizes = item.get_sizes()
-            if not sizes or args.retile == True:
+            if not sizes:
                 sizes = item.tile_image()
             manifest = item.create_manifest(sizes)
             upload_object_to_s3(manifest, item._id, f"iiif/{item._id}/manifest.json")
@@ -38,7 +30,7 @@ def main(args):  # ={"source": ITEMS_TO_PROCESS, "index": "all", "retile": False
                 ]
                 collection.add_item_by_reference(manifest)
             n_manifests += 1
-        except Exception as e:
+        except Exception:
             logger.exception(
                 f"{cf.RED}Couldn't create manifest for item {item._id}, skipping"
             )
@@ -49,35 +41,9 @@ def main(args):  # ={"source": ITEMS_TO_PROCESS, "index": "all", "retile": False
             collections[name], name, f"iiif/collection/{name.lower()}.json"
         )
 
-    summary = (
-        f"SUMMARY: Processing done. Parsed {cf.BLUE}{len(metadata)}{cf.RESET} "
-        f"items and created {cf.GREEN}{n_manifests}{cf.RESET} IIIF manifests. "
-    )
-    if no_collection:
-        summary += (
-            f"Items {cf.YELLOW}{no_collection}{cf.RESET} aren't associated with any collections. "
-            f"Please fill the Collection field in JSTOR so these images are displayed in imagineRio. "
-        )
-    if errors:
-        summary += (
-            f"Items {cf.RED}{errors}{cf.RESET} were skipped, likely due to issues with "
-            f"the images or metadata. Inspect the log above (with CTRL+F) for more details."
-        )
-    logger.info(summary)
-
-
-if __name__ == "__main__":
-    logger.info("Parsing arguments")
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--mode", "-m", help="run mode", choices=["test", "prod"], default="test"
-    )
-    parser.add_argument(
-        "--source", "-s", help="data source file", default=config.ITEMS_TO_PROCESS
-    )
-    parser.add_argument("--index", "-i", nargs="+", help="index to run", default="all")
-    parser.add_argument("--retile", "-r", action="store_true", default=False)
-    args = parser.parse_args()
-    if args.retile and args.index == "all":
-        parser.error("The --retile option cannot be used together with --index='all'")
-    main(args)
+    return {
+        "n_manifests": n_manifests,
+        "n_items": len(metadata),
+        "no_collection": no_collection,
+        "errors": errors,
+    }
