adb2722065d0f41208f3a7708d2c50e29d84c177
Martim Passos <martim.passos@alumni.usp.br>
2025-04-29 16:28:39 -0300

    Add local test and reprocess all

--- imaginerio-etl/scripts/update.py
+++ imaginerio-etl/scripts/update.py
@@ -1,15 +1,30 @@
 import argparse
 import os
 
-from ..config import *
-from ..utils.helpers import get_metadata_changes, summarize
+from ..config import (
+    CURRENT_JSTOR,
+    NEW_JSTOR,
+    KMLS_IN
+)
+from ..utils.helpers import get_metadata_changes, summarize, load_xls
 from ..utils.logger import logger
 from . import iiif, viewcones
 
 
 def main():
-    # Compare data, overwrite current data file if there are changes
-    all_data, changed_data = get_metadata_changes(CURRENT_JSTOR, NEW_JSTOR)
+    parser = argparse.ArgumentParser(description="Run JSTOR ETL update process.")
+    parser.add_argument("--id", help="Process only the row with this index from NEW_JSTOR")
+    args = parser.parse_args()
+
+    if args.id: # Run a single item for testing 
+        try:
+            changed_data = load_xls(NEW_JSTOR, "SSID").loc[[args.id]]
+            logger.info(f"Running ETL on row with ID: {args.id}")
+        except KeyError:
+            logger.error(f"ID '{args.id}' not found in NEW_JSTOR index.")
+            return
+    else: # Compare data, overwrite current data file if there are changes
+        all_data, changed_data = get_metadata_changes(CURRENT_JSTOR, NEW_JSTOR)
 
     # Update viewcones if any
     if any(file for file in os.listdir(KMLS_IN) if file != ".gitkeep"):
@@ -23,7 +38,7 @@ def main():
         logger.info("No metadata changes detected, exiting")
         manifest_info = None
     else:
-        manifest_info = iiif.update(changed_data)
+        manifest_info = iiif.update(changed_data, testing=args.id is not None)
 
     if viewcones_info or manifest_info:
         summary = summarize(viewcones_info, manifest_info)
